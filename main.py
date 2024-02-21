af56f4b56a2ea94310096e02437d4eb495a03a73
MrVt0rN1k <mrvtorn1k2354@gmail.com>
2024-02-21 14:59:25 +0300

    -

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+def get_result(url="https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=YourApiKeyToken"):
+    result = requests.get(url)
+    result_data = result.json()
+    result = result_data["result"]
+    return result
+
+
+def get_height(url="https://api.blockcypher.com/v1/eth/main"):
+    height = requests.get(url)
+    height_data = height.json()
+    height = height_data["height"]
+    return height
+
+
+comparison = int(get_result(), 16) == int(get_height())
+if get_result() == "Max rate limit reached, please use API Key for higher rate limit":
+    print("Max rate limit reached, please use API Key for higher rate limit")
+else:
+    print(comparison)
